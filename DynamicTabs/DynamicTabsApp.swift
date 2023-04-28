9effffc4906a257bd4ef116294ffa5e18403dd51
Max Valek <valek.maxwell@gmail.com>
2023-04-28 15:02:45 -0500

    Initial Commit

---
+++ DynamicTabs/DynamicTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabsApp.swift
+//  DynamicTabs
+//
+//  Created by Max Valek on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
